de437badd76cf3e2539353a9f5ed07603395559e
Mihai Tuță <tutamihai@gmail.com>
2021-10-07 18:48:49 +0200

    added _redirects file to fix netflix crash with vue route history mode

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex justify-center">
+    <q-card dark>
+      <q-card-section>
+        sms
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
 export default defineComponent({
   name: 'PageIndex'
 })
 </script>
+
+<style lang="scss" scoped>
+.q-card {
+  width: 100%;
+  height: inherit;
+  margin: 1rem;
+}
+
+
+@media (min-width: $breakpoint-sm-min) {
+  .q-card {
+    margin: 2rem;
+  }
+}
+
+@media (min-width: $breakpoint-md-min) {
+  .q-card {
+    margin: 3rem;
+  }
+}
+
+@media (min-width: $breakpoint-lg-min) {
+
+}
+</style>
